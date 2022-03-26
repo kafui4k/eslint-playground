4690e3e7cdcd45d4ba9ba1db824f7774a9b4553d
Kafui Alordo <kalordo7@gmail.com>
2022-03-26 13:19:52 +0000

    Formatting on Save

--- file.js
+++ file.js
@@ -1,8 +1,8 @@
-var foo = 'bard';
+var foo = "bard";
 
 foo;
 
-const Task = function(title, desc, dueDate) {
+const Task = function (title, desc, dueDate) {
   this.title = title;
   this.desc = desc;
   this.dueDate = dueDate;
@@ -10,10 +10,19 @@ const Task = function(title, desc, dueDate) {
   return {
     hello: () => {
       this.title;
-    }
+    },
   };
-
 };
 
-const newTask = new Task('test title', 'a description', 'today');
+const newTask = new Task("test title", "a description", "today");
 newTask.hello;
+
+const name = "James";
+
+const person = { first: name };
+
+console.log(person);
+
+const sayHelloLinting = (fName) => {
+  console.log(`Hello linting, ${fName}`);
+};
